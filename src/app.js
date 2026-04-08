f4f540eb76b2ea14ab4d788e1825382044ca3758
Rishi <rishi.777.patel@gmail.com>
2026-04-08 12:41:39 +0530

    utils code discussion

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send('<h2>Hello Rishi</h2>')
-})
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    })
+);
+
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
 
 export { app };
--- src/index.js
+++ src/index.js
@@ -4,37 +4,24 @@ import connectDB from "./db/index.js";
 import { app } from "./app.js";
 
 dotenv.config({
-  path: "./env",
+    path: "./env",
 });
+
+const port = process.env.PORT || 8000;
 
 connectDB()
 .then(() => {
     app.on("error",(error)=>{
             console.log("ERROR",error)
             throw error 
-        })
-    app.listen(process.env.PORT || 8000, () => {
-        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+    app.listen(port, () => {
+        console.log(`⚙️ Server is running at port : ${port}`);
     })
 })
 .catch((err)=>{
     console.log("MONGO db connection FAILED",err);
-
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
