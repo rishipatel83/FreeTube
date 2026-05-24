1b51f751dd2db58f37b5641abe546c9609f988d0
Rishi <rishi.777.patel@gmail.com>
2026-05-24 20:50:22 +0530

    adding login logout controller with auth middleware for jwt verification

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -10,7 +10,7 @@ export {asyncHandler}
 
 // const asyncHandler = (fn) => async (req,res,next) => {
 //   try {
-   
+//     return await fn(req, res, next)
 //   } catch (error) {
 //     res.status(err.code || 500).json({
 //       success: false,
